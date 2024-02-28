04b330dbb03e33d893c0604e728d07094b4da3c1
Yusuf <yusufsas3444@gmail.com>
2024-02-28 14:05:23 +0300

    listeleme

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,12 +2,31 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from bs4 import BeautifulSoup
+
+
+import requests
 
 
 def homepage(request):
-
+ 
     if request.method=='POST':
-        result=request.POST.get('arama')
-        return render(request,'index.html',{'result':result})
+        arama=request.POST.get('arama')
+
+        url=f'https://scholar.google.com/scholar?hl=tr&as_sdt=0%2C5&q={arama}&btnG='
+        response =requests.get(url)
+        html_content=response.text
+        soup=BeautifulSoup(html_content,'html.parser')
+        titles = soup.find_all(class_='gs_or_ggsm')
+        results=[]
+        for title in titles:
+            a=title.find('a')
+            if a:
+                span=a.find('span')
+                print(span.text)
+                results.append(title.text)
+
+           
+        return render(request,'index.html',{'results':results})
 
     return render(request,'index.html')
--- test/scarping.py
+++ test/scarping.py
@@ -4,9 +4,18 @@ from bs4 import BeautifulSoup
 
 import requests
 
-url='https://www.cnnturk.com/'
-response =requests.get(url)
 
+
+
+
+
+
+
+
+parameter='deep'
+
+url=f'https://scholar.google.com/scholar?hl=tr&as_sdt=0%2C5&q={parameter}&btnG='
+response =requests.get(url)
 html_content=response.text
 
 
@@ -15,7 +24,13 @@ html_content=response.text
 soup=BeautifulSoup(html_content,'html.parser')
 
 
-titles=soup.find('h1')
 
+titles = soup.find_all(class_='gs_or_ggsm')
 for title in titles:
-    print(title.text)
+
+    a=title.find('a')
+    if a:
+        span=a.find('span')
+        print(span.text)
+
+
